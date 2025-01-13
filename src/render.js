0aece95d6848387f96ef1e607e31b2adc63d7160
Sean <fengying1028@hotmail.com>
2025-01-13 09:30:17 -0600

    comments the recursion to optimize

--- src/render.js
+++ src/render.js
@@ -2,6 +2,8 @@ function render(element, container){
     //创建dom
     const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type);
     //看是否有子元素，如有就递归
+    //here is the doomed part, recursion, and we need to optimize it
+    //once we start the recursion, we can't stop it. it will stop the main thread
     element.props.children.forEach(item=>render(item,dom));
     //样式
     Object.keys(element.props).filter(item=>item!=='children').forEach(item=>dom[item]=element.props[item]);
