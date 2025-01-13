76fe003712ca1025c673ddee1d89b6731129606d
Sean <fengying1028@hotmail.com>
2025-01-12 20:58:32 -0600

    comment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import createElement from "./createElement";
 import crateTextElement from "./createTextElement";
 import render from "./render";
-export { createElement, crateTextElement, render }
+export { createElement, crateTextElement, render };
