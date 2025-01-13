50da0dc195217a6cd12521946d55eee2c1d6ab94
Sean <fengying1028@hotmail.com>
2025-01-13 14:18:41 -0600

    new render works

--- src/render.js
+++ src/render.js
@@ -10,182 +10,87 @@
 //     //挂载dom
 //     container.appendChild(dom);
 // }
-
+function createDom(fiber){
+    //创建dom
+    const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type);
+    //看是否有子元素，如有就递归
+    //here is the doomed part, recursion, and we need to optimize it
+    //once we start the recursion, we can't stop it. it will stop the main thread
+    // element.props.children.forEach(item=>render(item,dom));
+    //样式
+    Object.keys(fiber.props).filter(item=>item!=='children').forEach(item=>dom[item]=fiber.props[item]);
+    console.log(dom,'dom');
+    
+    return dom;
+    // //挂载dom
+    // container.appendChild(dom);
+}
 function render(element, container) {
-	wipRoot = {
+	nextUnitOfWork = {
 		dom: container,
 		props: {
 			children: [element],
 		},
-		alternate: currentRoot,
+        child: null,
 	};
-	deletions = [];
-	nextTask = wipRoot;
+
 }
 
-let nextTask = null;
-let wipRoot = null;
-let currentRoot = null;
-let deletions = null;
+let nextUnitOfWork = null;
 
 function workLoop(deadline) {
-	let shouldYield = false;
-	while (nextTask && !shouldYield) {
-		nextTask = performanceTask(nextTask);
-		//check the remaining time less than 1 ms, then yield
-		shouldYield = deadline.timeRemaining() < 1;
-	}
-	//once we finish, we commit the root
-	if (!nextTask && wipRoot) {
-		commitRoot();
-	}
-	requestIdleCallback(workLoop);
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        );
+        console.log(nextUnitOfWork);
+        
+        shouldYield = deadline.timeRemaining() < 1;// not enough time to operate
+    }
+    requestIdleCallback(workLoop);
 }
-requestIdleCallback(workLoop);
+requestIdleCallback(workLoop)//call this method will inject a parameter to the workLoop
 
-function commitRoot() {
-	// TODO add nodes to dom
-	deletions.forEach(commitWork);
-	commitWork(wipRoot.child);
-	currentRoot = wipRoot;
-	wipRoot = null;
-}
+function performUnitOfWork(fiber) {
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber);
+    }
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom);
+    }
+    const elements = fiber.props.children;
 
-const isEvent = (key) => key.startsWith("on");
-const isProperty = (key) => key !== "children" && !isEvent(key);
-const isNew = (prev, next) => (key) => prev[key] !== next[key];
-const isGone = (prev, next) => (key) => !(key in next);
-function updateDom(dom, prevProps, nextProps) {
-	//Remove old or changed event listeners
-	Object.keys(prevProps)
-		.filter(isEvent)
-		.filter((key) => !(key in nextProps) || isNew(prevProps, nextProps)(key))
-		.forEach((name) => {
-			const eventType = name.toLowerCase().substring(2);
-			dom.removeEventListener(eventType, prevProps[name]);
-		});
-	// Remove old properties
-	Object.keys(prevProps)
-		.filter(isProperty)
-		.filter(isGone(prevProps, nextProps))
-		.forEach((name) => {
-			dom[name] = "";
-		});
+    let prevSibling = null;
 
-	// Set new or changed properties
-	Object.keys(nextProps)
-		.filter(isProperty)
-		.filter(isNew(prevProps, nextProps))
-		.forEach((name) => {
-			dom[name] = nextProps[name];
-		});
-	// Add event listeners
-	Object.keys(nextProps)
-		.filter(isEvent)
-		.filter(isNew(prevProps, nextProps))
-		.forEach((name) => {
-			const eventType = name.toLowerCase().substring(2);
-			dom.addEventListener(eventType, nextProps[name]);
-		});
-}
-function commitWork(fiber) {
-	if (!fiber) return;
+    for (let i = 0; i < elements.length; i++) {
+        const newFiber = {
+            dom:null,
+            parent:fiber,
+            props:elements[i].props,
+            type:elements[i].type,
+            child: null,
+            sibling: null
+        }
+        if (i === 0) {
+            fiber.child = newFiber;
+        } else {
+            prevSibling.sibling = newFiber;
+        }
+        prevSibling = newFiber;
 
-	const domParent = fiber.parent.dom;
-	if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
-		domParent.appendChild(fiber.dom);
-	} else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
-		updateDom(fiber.dom, fiber.alternate.props, fiber.props);
-	} else if (fiber.effectTag === "DELETION") {
-		domParent.removeChild(fiber.dom);
-	}
-	domParent.appendChild(fiber.dom);
-	commitWork(fiber.child);
-	commitWork(fiber.sibling);
-}
-function performanceTask(fiber) {
-	// TODO add dom node
-	if (!fiber.dom) {
-		fiber.dom = createDom(fiber);
-	}
-	const elements = fiber.props.children;
-	reconcileChildren(fiber, elements);
-	//we might not be able to finish create dom, because the browser might interrupt us
-	// if (fiber.parent) {
-	// 	fiber.parent.dom.appendChild(fiber.dom);
-	// }
-	// TODO create new fibers
-	let index = 0;
-	let prevSibling = null;
+    }
 
-	while (index < elements.length) {
-		const element = elements[index];
-		const newFiber = {
-			type: element.type,
-			props: element.props,
-			parent: fiber,
-			dom: null,
-		};
-		if (index === 0) {
-			fiber.child = newFiber;
-		} else {
-			prevSibling.sibling = newFiber;
-		}
-		prevSibling = newFiber;
-		index++;
-	}
-	// TODO return next unit of work
-	if (fiber.child) {
-		return fiber.child;
-	}
-	let nextFiber = fiber;
-	while (nextFiber) {
-		if (nextFiber.sibling) {
-			return nextFiber.sibling;
-		}
-		nextFiber = nextFiber.parent;
-	}
+    if(fiber.child) return fiber.child;
+    let nextFiber = fiber;
+    while(nextFiber) {
+        //with sibling nodes
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        //else go parent node
+        nextFiber = nextFiber.parent;
+    }
 }
 
-function reconcileChildren(wipFiber, elements) {
-	let index = 0;
-	let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
-	let prevSibling = null;
-	while (index < elements.length || oldFiber != null) {
-		const element = elements[index];
-		let newFiber = null;
-		// TODO compare oldFiber to element
-		const sameType = oldFiber && element && element.type == oldFiber.type;
-		if (sameType) {
-			// TODO update the node
-			newFiber = {
-				type: oldFiber.type,
-				props: element.props,
-				dom: oldFiber.dom,
-				parent: wipFiber,
-				alternate: oldFiber,
-				effectTag: "UPDATE",
-			};
-		}
-		if (element && !sameType) {
-			// TODO add this node
-			newFiber = {
-				type: element.type,
-				props: element.props,
-				dom: null,
-				parent: wipFiber,
-				alternate: null,
-				effectTag: "PLACEMENT",
-			};
-		}
-		if (oldFiber && !sameType) {
-			// TODO delete the oldFiber's node
-			oldFiber.effectTag = "DELETION";
-			deletions.push(oldFiber);
-		}
-		if (oldFiber) {
-			oldFiber = oldFiber.sibling;
-		}
-	}
-}
 export default render;
